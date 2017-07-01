e11fa04e44bff0492857c8b282d3d19112881ff3
Caroline Vurlumis <cvurlumis@gmail.com>
2017-06-30 21:24:49 -0700

    Assignment-5-AddressBloc_Menu, added 'view entry number' option'

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
         main_menu
       when 2
         system "clear"
-        create_entry
+        view_entry_number
         main_menu
       when 3
         system "clear"
-        search_entries
+        create_entry
         main_menu
       when 4
         system "clear"
-        read_csv
+        search_entries
         main_menu
       when 5
+        system "clear"
+        read_csv
+        main_menu
+      when 6
         puts "Good-bye!"
 
         exit(0)
@@ -57,6 +62,24 @@ class MenuController
 
       system "clear"
       puts "End of entries"
+    end
+
+    def view_entry_number
+      print "Fetch entry number: "
+      entry_number = (gets.chomp.to_i) -1
+
+      if entry_number > -1 && entry_number < @address_book.entries.length
+        system "clear"
+        requested_entry = address_book.entries[entry_number]
+        main_menu
+      else
+        system "clear"
+        puts "That entry is invalid. Please choose an entry number below #{@address_book.entries.length}"
+        main_menu
+
+      end
+
+
     end
 
     def create_entry
